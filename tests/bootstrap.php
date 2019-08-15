81a9292d922b63f3591f4dc946c7701ccb74e7d6
Vladimir Catrici <vladimir.k89@gmail.com>
2019-08-15 23:04:49 +1200

    Path to classes fixed for tests

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,7 +1,7 @@
 <?php
-require __DIR__ . '/../src/Shopify/API.php';
-require __DIR__ . '/../src/Shopify/Collection.php';
-require __DIR__ . '/../src/Shopify/Exceptions/RequestException.php';
+require __DIR__ . '/../src/API.php';
+require __DIR__ . '/../src/Collection.php';
+require __DIR__ . '/../src/Exceptions/RequestException.php';
 
 $dotenv = Dotenv\Dotenv::create(__DIR__ . '/../');
 $dotenv->load();
