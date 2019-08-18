5eceb832c8feeeffbde5db995f35cd74856e4b00
Vladimir Catrici <vladimir.k89@gmail.com>
2019-08-18 15:52:11 +1200

    Cursor-based pagination support

--- src/Collection.php
+++ src/Collection.php
@@ -18,6 +18,13 @@ class Collection implements Iterator, Countable, ArrayAccess {
     private $limit = 250;
 
     private $page = 1;
+    /**
+     * @var string A unique ID used to access a certain page of results. The page_info parameter can't be modified and
+     * must be used exactly as it appears in the link header URL.
+     * e.g. header:
+     * Link: "<https://{shop}.myshopify.com/admin/api/2019-07/products.json?page_info=hijgklmn&limit=3>; rel=next"
+     */
+    private $nextPageInfo;
 
     private $numPages;
 
@@ -30,6 +37,37 @@ class Collection implements Iterator, Countable, ArrayAccess {
     private $count;
 
     private $currentIndex = 0;
+
+    private $isCursorBasedPagination = false;
+
+    private $cursorBasedPaginationEndpoints = [
+        /**
+         * According to: https://help.shopify.com/en/api/versioning/release-notes/2019-07
+         */
+        '2019-07' => [
+            'article_saved_searches', 'balance_transaction_saved_searches', 'blog_saved_searches',
+            'checkout_saved_searches', 'collects', 'collection_listings',
+            'collection_listings\/%d+\/product_ids', 'collections', 'collection_saved_searches',
+            'comment_saved_searches', 'customer_saved_searches', 'discount_code_saved_searches',
+            'draft_order_saved_searches', 'events', 'file_saved_searches', 'gift_card_saved_searches',
+            'inventory_transfer_saved_searches', 'metafields', 'page_saved_searches', 'products', 'search',
+            'product_listings', 'product_saved_searches', 'variants\/search', 'product_variant_saved_searches',
+            'redirect_saved_searches', 'transfer_saved_searches'
+        ],
+
+        /**
+         * According to: https://help.shopify.com/en/api/versioning/release-notes/2019-10
+         */
+        '2019-10' => [
+            'blogs\/\d+\/articles', 'blogs', 'comments', 'custom_collections', 'customers\/\d+\/addresses',
+            'customers', 'customers\/search', 'price_rules\/\d+\/discount_codes', 'shopify_payments\/disputes',
+            'draft_orders', 'orders\/\d+\/fulfillments', 'gift_cards', 'gift_cards\/search', 'inventory_items',
+            'inventory_levels', 'locations\/\d+\/inventory_levels', 'marketing_events', 'orders',
+            'orders\/\d+\/risks', 'pages', 'shopify_payments\/payouts', 'price_rules\/product_ids',
+            'product_listings\/product_ids', 'variants', 'redirects', 'orders\/\d\/refunds', 'reports', 'script_tags',
+            'smart_collections', 'tender_transactions', 'shopify_payments\/balance\/transactions', 'webhooks'
+        ]
+    ];
 
     /**
      * Collection constructor.
@@ -50,6 +88,8 @@ class Collection implements Iterator, Countable, ArrayAccess {
 
         $this->count = $this->api->get($this->endpoint . '/count', $this->options);
         $this->numPages = ceil($this->count / $this->limit);
+
+        $this->setPaginationType();
     }
 
     /**
@@ -69,9 +109,15 @@ class Collection implements Iterator, Countable, ArrayAccess {
      */
     public function next() {
         $this->partIndex++;
-        if ($this->partIndex == $this->limit && $this->page < $this->numPages) {
-            $this->page++;
-            $this->fetch();
+        if ($this->partIndex == $this->limit) {
+            if ($this->isCursorBasedPagination) {
+                if (!empty($this->nextPageInfo)) {
+                    $this->fetch();
+                }
+            } elseif ($this->page < $this->numPages) {
+                $this->page++;
+                $this->fetch();
+            }
         }
         $this->currentIndex++;
     }
@@ -98,12 +144,19 @@ class Collection implements Iterator, Countable, ArrayAccess {
     }
 
     /**
+     * @deprecated Cannot be used if Shopify API version switched to cursor-based pagination for this endpoint.
+     * Cursor-based pagination released in 2019-07 for some endpoints, and more endpoints to be added in 2019-10.
+     * More details: https://help.shopify.com/en/api/guides/paginated-rest-results
      * @param mixed $offset
      * @return mixed|null
      * @throws API\RequestException
      * @throws GuzzleException
      */
     public function offsetGet($offset) {
+        if ($this->isCursorBasedPagination) {
+            throw new LogicException('ArrayAccess cannot be used for endpoints switched to cursor-based pagination');
+        }
+        trigger_error('ArrayAccess interface read operations are deprecated for \VladimirCatrici\Shopify\Collection objects', E_USER_DEPRECATED);
         if (!is_int($offset) || $offset < 0 || $offset >= $this->count) {
             return null;
         }
@@ -117,6 +170,10 @@ class Collection implements Iterator, Countable, ArrayAccess {
     }
 
     public function offsetExists($offset) {
+        if ($this->isCursorBasedPagination) {
+            throw new LogicException('ArrayAccess cannot be used for endpoints switched to cursor-based pagination');
+        }
+        trigger_error('ArrayAccess interface read operations are deprecated for \VladimirCatrici\Shopify\Collection objects', E_USER_DEPRECATED);
         return is_int($offset) && $offset >= 0 && $offset < $this->count;
     }
 
@@ -134,12 +191,44 @@ class Collection implements Iterator, Countable, ArrayAccess {
      * @throws API\RequestException
      */
     private function fetch() {
-        $this->items = $this->api->get($this->endpoint, [
-            'limit' => $this->limit,
-            'page' => $this->page
-        ] + $this->options);
+        $options = [
+            'limit' => $this->page
+        ];
+        if ($this->isCursorBasedPagination) {
+            /**
+             * Cursor-based pagination accepts other parameters rather than limit only in the first request.
+             * To get next pages need you need only pass `limit` and `page_info` parameters.
+             */
+            if ($this->page == 1) {
+                $options += $this->options;
+            } else {
+                $options['page_info'] = $this->nextPageInfo;
+            }
+        } else {
+            $options['page'] = $this->page;
+            $options += $this->options;
+        }
+        $this->items = $this->api->get($this->endpoint, $options);
         $this->fetched = true;
         $this->partIndex = 0;
+        if ($this->isCursorBasedPagination) {
+            $this->updateNextPageInfo();
+        }
+    }
+
+    private function updateNextPageInfo() {
+        if (!array_key_exists('Link', $this->api->respHeaders)) { // All results on the same page
+            $this->nextPageInfo = null;
+            return;
+        }
+        $linkHeaderValue = $this->api->respHeaders['Link'][0];
+        preg_match('/page_info=([^>]+)>;\srel="next"/i', $linkHeaderValue, $matches);
+        if (empty($matches[1])) { // No next page
+            $this->nextPageInfo = null;
+            return;
+        }
+
+        $this->nextPageInfo = $matches[1];
     }
 
     private function offset2page($offset) {
@@ -148,5 +237,24 @@ class Collection implements Iterator, Countable, ArrayAccess {
 
     private function offset2partIndex($offset) {
         return $offset < $this->limit ? $offset : $offset % $this->limit;
+    }
+
+    private function setPaginationType() {
+        $apiVersion = $this->api->getVersion();
+        if ($apiVersion == '2019-04') {
+            $this->isCursorBasedPagination = false;
+            return;
+        }
+
+        foreach ($this->cursorBasedPaginationEndpoints as $version => $endpointsRegEx) {
+            if ($version <= $apiVersion) {
+                foreach ($endpointsRegEx as $re) {
+                    if (preg_match('/' . $re . '/', $this->endpoint)) {
+                        $this->isCursorBasedPagination = true;
+                        break 2;
+                    }
+                }
+            }
+        }
     }
 }
--- tests/CollectionTest.php
+++ tests/CollectionTest.php
@@ -38,7 +38,7 @@ class CollectionTest extends TestCase {
      * @throws GuzzleException
      */
     public function testCount() {
-        self::$mock->append(new Response(200, [], '{"count": 1001}'));
+        self::$mock->append(new Response(200, ['X-Shopify-API-Version' => '2019-04'], '{"count": 1001}'));
         $collection = new Collection(self::$api, 'products');
         $this->assertEquals(1001, count($collection));
         $this->assertEquals(1001, $collection->count());
@@ -161,13 +161,143 @@ class CollectionTest extends TestCase {
     }
 
     /**
+     * @throws API\RequestException
+     * @throws GuzzleException
+     */
+    public function testArrayAccessOffsetExistsThrowsExceptionForCollectionsBasedOnCursorPagination() {
+        $mock = new MockHandler();
+        $mock->append(new Response(200, [], '{"count": 2}'));
+        $mock->append(new Response(200, [], '{"products": [{"id": 1, "title": "Product 1"}, {"id": 2, "title": "Product 2"}]}'));
+        $handler = HandlerStack::create($mock);
+        $api = new API('test', 'test', [
+            'api_version' => '2019-07',
+            'handler' => $handler
+        ]);
+        $products = new Collection($api, 'products');
+        $this->expectException(LogicException::class);
+        /** @noinspection PhpExpressionResultUnusedInspection */
+        isset($products[0]);
+    }
+
+    /**
+     * @throws API\RequestException
+     * @throws GuzzleException
+     */
+    public function testCursorBasedPagination() {
+        self::$api->setVersion('2019-07');
+        $this->mockCollection(3, 20, true, true);
+        $products = new Collection(self::$api, 'products', ['limit' => 20]);
+        $this->assertEquals(60, count($products));
+        $numProducts = 0;
+        foreach ($products as $key => $product) {
+            $numProducts++;
+            $this->assertEquals(1000 + $key, $product['id']);
+        }
+        $this->assertEquals(60, $numProducts);
+    }
+
+    /**
+     * @throws API\RequestException
+     * @throws GuzzleException
+     */
+    public function testCursorBasedPaginationWithoutPages() {
+        self::$api->setVersion('2019-07');
+        $this->mockCollection(1, 5, true, true);
+        $products = new Collection(self::$api, 'products', ['limit' => 5]);
+        $this->assertEquals(5, count($products));
+        $numProducts = 0;
+        foreach ($products as $key => $product) {
+            $numProducts++;
+            $this->assertEquals(1000 + $key, $product['id']);
+        }
+        $this->assertEquals(5, $numProducts);
+    }
+
+    /**
+     * @dataProvider cursorBasedPaginationEndpoints
+     * @param $apiVersion
+     * @param $endpoint
+     * @return void [$apiVersion, $endpoint] to be used by testCursorBasedPaginationThrowsExceptionOnArrayAccess
+     * @throws API\RequestException
+     * @throws GuzzleException
+     */
+    public function testArrayAccessReadOperationsWorkInEligibleApiVersions($apiVersion, $endpoint) {
+        $mock = new MockHandler();
+        $mock->append(new Response(200, [], '{"count": 2}'));
+        $mock->append(new Response(200, [], '{"products": [{"id": 1, "title": "Product 1"}, {"id": 2, "title": "Product 2"}]}'));
+        $handler = HandlerStack::create($mock);
+        $api = new API('test', 'test', [
+            'api_version' => '2019-04',
+            'handler' => $handler,
+            'max_attempts_on_server_errors' => 1,
+            'max_attempts_on_rate_limit_errors' => 1
+        ]);
+        $products = new Collection($api, $endpoint, ['limit' => 2]);
+        $this->assertEquals('Product 1', $products[0]['title']);
+    }
+
+    /**
+     * @dataProvider cursorBasedPaginationEndpoints
+     * @param $apiVersion
+     * @param $endpoint
+     * @throws API\RequestException
+     * @throws GuzzleException
+     */
+    public function testCursorBasedPaginationThrowsExceptionOnArrayAccess($apiVersion, $endpoint) {
+        $mock = new MockHandler();
+        $mock->append(new Response(200, [], '{"count": 100}'));
+        $handler = HandlerStack::create($mock);
+        $api = new API('test', 'test', [
+            'api_version' => $apiVersion,
+            'handler' => $handler,
+            'max_attempts_on_server_errors' => 1,
+            'max_attempts_on_rate_limit_errors' => 1
+        ]);
+        $products = new Collection($api, $endpoint, ['limit' => 2]);
+        $this->expectException(LogicException::class);
+        $products[0];
+    }
+
+    public function cursorBasedPaginationEndpoints() {
+        $output = [];
+        $data = [
+            '2019-07' => [
+                'article_saved_searches', 'balance_transaction_saved_searches', 'blog_saved_searches',
+                'checkout_saved_searches', 'collects', 'collection_listings',
+                'collection_listings/1234567890/product_ids', 'collections', 'collection_saved_searches',
+                'comment_saved_searches', 'customer_saved_searches', 'discount_code_saved_searches',
+                'draft_order_saved_searches', 'events', 'file_saved_searches', 'gift_card_saved_searches',
+                'inventory_transfer_saved_searches', 'metafields', 'page_saved_searches', 'products', 'search',
+                'product_listings', 'product_saved_searches', 'variants\/search', 'product_variant_saved_searches',
+                'redirect_saved_searches', 'transfer_saved_searches'
+            ],
+            '2019-10' => [
+                'blogs/1234567890/articles', 'blogs', 'comments', 'custom_collections', 'customers/1234567890/addresses',
+                'customers', 'customers/search', 'price_rules/1234567890/discount_codes', 'shopify_payments/disputes',
+                'draft_orders', 'orders/1234567890/fulfillments', 'gift_cards', 'gift_cards/search', 'inventory_items',
+                'inventory_levels', 'locations/1234567890/inventory_levels', 'marketing_events', 'orders',
+                'orders/1234567890/risks', 'pages', 'shopify_payments/payouts', 'price_rules/product_ids',
+                'product_listings/product_ids', 'variants', 'redirects', 'orders/1234567890/refunds', 'reports', 'script_tags',
+                'smart_collections', 'tender_transactions', 'shopify_payments/balance/transactions', 'webhooks'
+            ]
+        ];
+        foreach ($data as $apiVersion => $endpoints) {
+            foreach ($endpoints as $endpoint) {
+                $output[] = [$apiVersion, $endpoint];
+            }
+        }
+        return $output;
+    }
+
+    /**
      * Generates and adds to the Mock object list of responses for the Collection object.
      * @param $numPages
      * @param $limit
      * @param bool $includeCountResponse Whether the count response should be added as the first response. BY default
      * it's set to TRUE, might be useful when you need to set it to FALSE, e.g. testArrayAccessInterface();
+     * @param bool $cursorBasedPagination
      */
-    private function mockCollection($numPages, $limit, $includeCountResponse = true) {
+    private function mockCollection($numPages, $limit, $includeCountResponse = true, $cursorBasedPagination = false) {
         if ($includeCountResponse) {
             self::$mock->append(new Response(200, [], '{"count": ' . $numPages * $limit . '}'));
         }
@@ -182,7 +312,30 @@ class CollectionTest extends TestCase {
                 $id++;
             }
             $pageProductsJson = json_encode(['products' => $pageProducts]);
-            self::$mock->append(new Response(200, [], $pageProductsJson));
+            $headers = [];
+            if ($cursorBasedPagination) {
+                $links = [];
+                if ($page > 0) {
+                    $links[] = '<https://hilditchandkey.myshopify.com/admin/api/2019-07/products.json?limit=20&page_info=' . $this->rndStr() . '>; rel="previous"';
+                }
+                if ($page < ($numPages - 1)) {
+                    $links[] = '<https://hilditchandkey.myshopify.com/admin/api/2019-07/products.json?limit=20&page_info=' . $this->rndStr() . '>; rel="next"';
+                }
+                if (count($links) > 0) {
+                    $headers['Link'] = implode(',', $links);
+                }
+            }
+            self::$mock->append(new Response(200, $headers, $pageProductsJson));
         }
+    }
+
+    private function rndStr($length = 12) {
+        $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $charactersLength = strlen($characters);
+        $randomString = '';
+        for ($i = 0; $i < $length; $i++) {
+            $randomString .= $characters[rand(0, $charactersLength - 1)];
+        }
+        return $randomString;
     }
 }
