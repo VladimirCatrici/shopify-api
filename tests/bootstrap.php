d1c0a0bc7e578d713e3242e6b1274c11b7955f30
Vladimir Catrici <vladimir.k89@gmail.com>
2019-10-31 23:26:55 +1300

    Adjustments to be able run tests on Travis

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,5 +4,11 @@ require __DIR__ . '/../src/Collection.php';
 require __DIR__ . '/../src/Exception/RequestException.php';
 require __DIR__ . '/../src/PaginationType.php';
 
-$dotenv = Dotenv\Dotenv::create(__DIR__ . '/../');
-$dotenv->load();
+if (file_exists(__DIR__ . '/../.env')) {
+    /*
+     * This file is missing on running tests in Travis.
+     * The file_exists() condition added to avoid Exception.
+     */
+    $dotenv = Dotenv\Dotenv::create(__DIR__ . '/../');
+    $dotenv->load();
+}
