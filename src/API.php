50d53c25d70f7c107d5f1cb5c0ba6cc1f62adf13
Vladimir Catrici <vladimir.k89@gmail.com>
2019-11-04 00:43:14 +1300

    Revert "Refactoring"
    This reverts commit e077f870c15b057496c8bd43d8fb4c3fc293dd0b.

--- src/API.php
+++ src/API.php
@@ -281,7 +281,7 @@ class API {
 
         if ($key == 'api_version') {
             $value = trim($value);
-            Validator::validateApiVersion($value);
+            $this->validateApiVersion($value);
 
             // Update client config
             if (!empty($this->client)) {
@@ -297,6 +297,31 @@ class API {
         }
 
         $this->options[$key] = $value;
+    }
+
+    private function validateApiVersion($value) {
+        $readMoreAboutApiVersioning = 'Read more about versioning here: https://help.shopify.com/en/api/versioning';
+        if (!preg_match('/^(\d{4})-(\d{2})$/', $value, $matches)) {
+            throw new InvalidArgumentException(
+                sprintf('Invalid API version format: "%s". The "YYYY-MM" format expected. ' . $readMoreAboutApiVersioning,
+                    $value)
+            );
+        }
+        if ($matches[1] < 2019) {
+            throw new InvalidArgumentException(
+                sprintf('Invalid API version year: "%s". The API versioning has been released in 2019. ' . $readMoreAboutApiVersioning,
+                    $value)
+            );
+        }
+        if (!preg_match('/01|04|07|10/', $matches[2])) {
+            throw new InvalidArgumentException(
+                sprintf('Invalid API version month: "%s". 
+                    The API versioning has been released on April 2019 and new releases scheduled every 3 months, 
+                    so only "01", "04", "07" and "10" expected as a month. Otherwise, 
+                    "404 Not Found" will be returned by Shopify. ' . $readMoreAboutApiVersioning,
+                        $matches[0])
+            );
+        }
     }
 
     public function setVersion($version) {
