03be8a92151b862a8d36e40c65bae39bc8ad1711
Vladimir Catrici <vladimir.k89@gmail.com>
2019-11-14 22:50:14 +1300

    Formatting

--- src/ClientInterface.php
+++ src/ClientInterface.php
@@ -5,10 +5,7 @@ namespace VladimirCatrici\Shopify;
 interface ClientInterface
 {
     public function get(string $endpoint, array $query = []);
-
     public function post(string $endpoint, array $data = []);
-
     public function put(string $endpoint, array $data = []);
-
     public function delete(string $endpoint);
 }
