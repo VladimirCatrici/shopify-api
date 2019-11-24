06bb1970dde602d2bdc92dbc33c5dfc96d7fb654
Vladimir Catrici <vladimir.k89@gmail.com>
2019-11-24 22:50:06 +1300

    RequestException null response fix

--- src/Exception/RequestException.php
+++ src/Exception/RequestException.php
@@ -34,7 +34,8 @@ class RequestException extends Exception
         $this->client = $client;
         $this->request = $previous->getRequest();
         $this->response = $previous->getResponse();
-        parent::__construct((string) $this->response->getBody(), $this->response->getStatusCode(), $previous);
+        $body = $previous->hasResponse() ? (string) $this->response->getBody() : '';
+        parent::__construct($body, $this->response->getStatusCode(), $previous);
     }
 
     /**
@@ -70,11 +71,11 @@ class RequestException extends Exception
                 'headers' => $this->request->getHeaders(),
                 'body' => (string) $this->request->getBody()
             ],
-            'response' => [
+            'response' => $this->response !== null ? [
                 'code' => $this->response->getStatusCode(),
                 'body' => (string) $this->response->getBody(),
                 'headers' => $this->response->getHeaders()
-            ]
+            ] : ''
         ]);
     }
 }
